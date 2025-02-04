7e5cb3da023bcc1e2cce1217b20ced4822be2604
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-02-04 09:51:01 -0800

    testing

---
+++ index.js
@@ -0,0 +1,7 @@
+let CertificateOne = document.getElementById("living");
+let CertificateTwo = document.getElementById("average");
+let CertificateThree = document.getElementById("boring");
+
+CertificateOne.addEventListener("click", (e) => {
+    CertificateOne.style.border = "3px blue solid";
+});
