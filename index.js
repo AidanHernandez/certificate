c1835734a4cd18b9901a93c6c4f729faf19aa3f5
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-02-05 09:51:34 -0800

    cooking

--- index.js
+++ index.js
@@ -1,7 +1,79 @@
-let CertificateOne = document.getElementById("living");
-let CertificateTwo = document.getElementById("average");
-let CertificateThree = document.getElementById("boring");
 
-CertificateOne.addEventListener("click", (e) => {
-    CertificateOne.style.border = "3px blue solid";
+let ChoiceOne = document.getElementById("choiceOne");
+let choiceTwo = document.getElementById("choiceTwo");
+let choiceThree = document.getElementById("choiceThree");
+let main = document.getElementById("certificate");
+let name;
+let chose;
+
+
+ChoiceOne.addEventListener("mouseleave", (e) => {
+    name = ChoiceOne;
+    leave();
 });
+
+ChoiceOne.addEventListener("mouseenter", (e) => {
+    name = ChoiceOne;
+    enter();
+});
+
+choiceTwo.addEventListener("mouseleave", (e) => {
+    name = choiceTwo;
+    leave();
+});
+
+choiceTwo.addEventListener("mouseenter", (e) => {
+    name = choiceTwo;
+    enter();
+});
+
+choiceThree.addEventListener("mouseleave", (e) => {
+    name = choiceThree;
+    leave();
+});
+
+choiceThree.addEventListener("mouseenter", (e) => {
+    name = choiceThree;
+    enter();
+});
+
+
+
+function leave(){
+    name.classList.add("left");
+    name.classList.remove("enter");
+    setTimeout(() =>  name.classList.remove("left"),3000);
+}
+
+function enter(){
+    name.classList.add("enter");
+    name.classList.remove("left");
+}
+
+
+
+
+
+
+ChoiceOne.addEventListener("click", (e) => {
+    chose = "living";
+    chosen()
+});
+
+function chosen(){
+    let gone = document.querySelector("h1");
+    gone.style.display = "none";
+    gone = document.querySelector("h2");
+    gone.style.display = "none";
+    gone = document.querySelector("div");
+    gone.style.display = "none";
+
+    main.style.width = "1100px";
+    main.style.height = "850px";
+
+    switch(chose){
+        case "living":
+            main.style.backgroundColor = "white";
+            
+    }
+}
